26921a77021a948506c5991fb4fd6631223319ba
Jason Fowler <jason@kissmycreative.com>
2025-01-28 14:04:39 +0800

    testing build

--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"runtime"
+
+	"github.com/SalaciousSystems/studious-rotary-phone/internal/app"
 )
 
 func main() {
-	fmt.Printf("Running on %s_%s", runtime.GOOS, runtime.GOARCH)
+	fmt.Printf("Running %s on %s_%s", app.GetBuildMeta(), runtime.GOOS, runtime.GOARCH)
 }
